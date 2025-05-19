9f5a7607d1920210faa6565ac764882e1b0df00e
mauroanastasi <mauroanastasi1@gmail.com>
2025-05-19 21:45:50 +0200

    [UPD] definisco componente PostsPage

---
+++ components/PostsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PostsList from './PostsList'
+
+const PostsPage = () => {
+    return (
+        <>
+            <h1>ELENCO POSTS</h1>
+            <PostsList />
+        </>
+    )
+}
+
+export default PostsPage
